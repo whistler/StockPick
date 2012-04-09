d1420dc0c3821ef861b007ed20bda044c3f8043c
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2012-04-09 19:46:35 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stockpick::Application.config.secret_token = '5bc194113cf36c4fe54b7f8c96f3df7b302ab4ced6fadd51e8b61aa9334f3a4b7b8bb26ee878956a39b6cbe7a9ea715630a5041283c5e944c9a20ce97d9854d5'
